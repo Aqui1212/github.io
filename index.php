fca4ef0fe884f8afb809f86c3029c227770cd158
Aqui1212 <164016899+Aqui1212@users.noreply.github.com>
2024-03-20 04:50:55 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,62 +1,173 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <meta charset="UTF-8">
-  <link rel="stylesheet" type="text/css" href="./css/onepage-scroll.css">
-  <link rel="stylesheet" type="text/css" href="./css/style2.css">
-	<script type="text/javascript" src="./js/jquery-1.11.3.min.js"></script>
-  <script type="text/javascript" src="./js/jquery.onepage-scroll.min.js"></script>
+<meta charset="UTF-8" />
+<title>report</title>
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<link rel="stylesheet" href="css/style.css">
+<input type="date">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<style type="text/css">
+.nav {
+	overflow: hidden;
+	width: 1250px;    
+        margin: 0 auto;
+	padding: 0;
+	display: flex;
+}
+
+.nav li {
+	list-style: none;
+}
+
+.nav li a {
+	display: block;
+	width: 250px;
+	height: 40px;
+	text-align: center;
+	color: white;
+	font-size: 14px;
+	line-height: 2.8;
+	background: gray;
+	text-decoration: none;
+	box-sizing: border-box;
+}
+
+.nav>li:hover>a {
+	color: brown;
+}
+
+.nav li ul {
+	width: 200px;
+	display: none;
+	margin-left: -40px;
+	position: absolute;
+}
+
+.nav>li li:hover>a {
+	color: gray;
+}
+
+.nav li ul li a {
+	border-top: 1px solid #eee;
+}
+
+.content {
+	width: 1280px;
+	height: 480px;
+	background: #eee;
+	margin: 0 auto;
+	padding: 10px;
+	text-align: left;
+	box-sizing: border-box;
+}
+</style>
+<style>
+table {
+    margin-left: auto;
+    margin-right: auto;
+}
+</style>
+	<ul class="nav">
+		<li><a href="#">Aqui</a></li>
+		<li><a href="#">mypage</a>
+		   <ul><li><a href="#">Log in</a></li></ul></li>
+		<li><a href="#">develop</a>
+		   <ul><li><a href="#">develop</a></li></ul></li>
+                <li><a href="#">develop</a></li>
+		<li><a href="#">develop</a></li>
+		<li><a href="#">develop</a></li>
+	</ul>
+        <script>
+		$(function() {
+			var nav = $('.nav');
+			$('li', nav).mouseover(function(e) {
+				$('ul', this).stop().slideDown('fast');
+			}).mouseout(function(e) {
+				$('ul', this).stop().slideUp('fast');
+			});
+		});
+	</script>
 </head>
-<body class="loading">
-  <div class="main">
-    <section>
-      <div class="container">
-        <h1>Welcome!</h1>
-      </div>
-    </section>
-    <section>
-      <div class="container">
-        <div class="left col">
-      <h1 class="slideInUp">Are you ready to ...</h1>
-          <p class="slideInUp"></p>
-          <p class="slideInUp">
-          </p>
-   <h1><a href="/index1.php">go to site!</a></h1>
+<body>
 
-  <!--   <h6><a href="/July1/">ポートフォリオ(1)</a></h6>
-<h6><a href="/Aug6/staff_login/staff_top.php">ポートフォリオ(2)</a></h6> -->
+<header>
+<?php 
+$logfile = "./counter.log";
+$counter = 0;
+if( $fn = @fopen($logfile, 'r') ) {
+ $counter = trim(fgets($fn));
+ fclose($fn);
+}
+print( ++$counter . "人目のお客様です<br>");
+ if( $fn = @fopen($logfile, 'w') ) {
+ fwrite( $fn, $counter);
+ fclose($fn);
+}?>
+ <form class="navbar-form" role="search">
+        <div class="input-group">
+<div style="text-align: center;"><img src="images/mega.jpg" width="600" height="400" alt="">
+        </div><ul class="nav"></nav>
+</div>
+</header>
 
-        </div>
-        <div class="right col">
-          <img src="./img/img-laptop.png" class="slideInUp" />
-        </div>
-      </div>
-    </section>
-  </div>
-  <script type="text/javascript">
-    $(document).ready(function() {
-      $(".main").onepage_scroll({
-        sectionContainer: "section",     // sectionContainer accepts any kind of selector in case you don't want to use section
-        easing: "cubic-bezier(0.080, 0.545, 0.170, 1.000)",                  // Easing options accepts the CSS3 easing animation such "ease", "linear", "ease-in",
-                                         // "ease-out", "ease-in-out", or even cubic bezier value such as "cubic-bezier(0.175, 0.885, 0.420, 1.310)"
-        animationTime: 800,             // AnimationTime let you define how long each section takes to animate
-        pagination: true,                // You can either show or hide the pagination. Toggle true for show, false for hide.
-        updateURL: false,                // Toggle this true if you want the URL to be updated automatically when the user scroll to each page.
-        beforeMove: function(index) {},  // This option accepts a callback function. The function will be called before the page moves.
-        afterMove: function(index) {},   // This option accepts a callback function. The function will be called after the page moves.
-        loop: false,                     // You can have the page loop back to the top/bottom when the user navigates at up/down on the first/last page.
-        keyboard: true,                  // You can activate the keyboard controls
-        responsiveFallback: false,        // You can fallback to normal page scroll by defining the width of the browser in which
-        // you want the responsive fallback to be triggered. For example, set this to 600 and whenever
-        // the browser's width is less than 600, the fallback will kick in.
-        direction: "vertical"            // You can now define the direction of the One Page Scroll animation. Options available are "vertical" and "horizontal". The default value is "vertical".
-      });
-    });
-    $(window).load(function() {
-      window.setTimeout(function(){
-        $('body').removeClass("loading").addClass('complete');
-      }, 500);
-    });
-  </script>
+<div class="content">
+   <form method="get" action="select.html">
+        <table border="1" 　cellspadding="10">
+            <tr>
+                <caption>situation</caption>
+                <th colspan="2">now</th>
+                <td class="rainy">
+                    <script type="text/javascript"> 
+                        var weeks = new Array('日', '月', '火', '水', '木', '金', '土');
+                        var now = new Date();
+                        var year = now.getYear(); // 年
+                        var month = now.getMonth() + 1; // 月
+                        var day = now.getDate(); // 日
+                        var week = weeks[now.getDay()]; // 曜日
+                        var hour = now.getHours(); // 時
+                        var min = now.getMinutes(); // 分
+                        var sec = now.getSeconds(); // 秒
+                        if (year < 2000) { year += 1900; }
+                        // 数値が1桁の場合、頭に0を付けて2桁で表示する指定
+                        if (month < 10) { month = "0" + month; }
+                        if (day < 10) { day = "0" + day; }
+                        if (hour < 10) { hour = "0" + hour; }
+                        if (min < 10) { min = "0" + min; }
+                        if (sec < 10) { sec = "0" + sec; }
+                        // 表示開始
+                        document.write('day time：<b>' + year + '年' + month + '月' + day + '日（' + week + '）');
+                        document.write(hour + '時' + min + '分' + sec + '秒</b>');
+                        // 表示終了
+                        // -->
+                    </script>
+          </td>
+            <tr>
+                <th colspan="2">where</th>
+                <td class="rainy">
+                    <div class="map-wrapper">
+                        <div class="googlemap">
+                            <iframe
+                                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d207578.68804604618!2d139.99962151640625!3d35.6174451!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60228531b1451f71%3A0xca8d10b88023fab4!2z5qCq5byP5Lya56S-IOOCr-ODquOCqOOCpOODiOOCqOODs-ODh-OCow!5e0!3m2!1sja!2sjp!4v1627543014714!5m2!1sja!2sjp"
+                                width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+                        </div>
+                    </div>
+                </td>
+            </tr>
+            <form method="post" action="check.php"></form>  
+        </table>
+    </form>
+
+
+<footer>
+<div class="card_body" style="display: gray;">
+<span class="msft">&#169; 2024 </span>
+<ul class="items"><li class="item">
+<a id="privacy" href="//go.microsoft.com/fwlink/?LinkId=521839" role="link" class="">
+
+</footer>
+
 </body>
 </html>
+
+                
